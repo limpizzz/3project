54ba28ff56e12a9647f7d8f6386b4ece5c84fbe0
limpizzz <152085829+limpizzz@users.noreply.github.com>
2024-05-22 13:49:13 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 token = '6793082893:AAEzwEgPYk14p8E1sp6bsAouiTStf2NuA00'
 folder_id = 'b1gb1o17aj4e22s1c3m4'
-iam = 't1.9euelZrJnsacx8vIkouXl4qRyo-anu3rnpWajZmMmcqTisnKyJPOjYmVlpnl8_dFZ2lN-e8FSFV9_d3z9wUWZ0357wVIVX39zef1656VmpeSypyNy46UyJmUjZ3IkpOL7_zF656VmpeSypyNy46UyJmUjZ3IkpOLveuelZqKyceVmJyJx8-WzJaZy86em7XehpzRnJCSj4qLmtGLmdKckJKPioua0pKai56bnoue0oye.Z4mnkkzaOiUeAZSM--XXVsqpxiyIXWvI3m1I9MDDfi48ubMm3EDnsdpJoVyRBy_deLjkaJmqHxoO-D3TcDK-Bg'
+iam = 't1.9euelZqWmJSajIvMjpaby5nKjcaNle3rnpWajZmMmcqTisnKyJPOjYmVlpnl9Pd6fUhN-e9-FEmZ3fT3OixGTfnvfhRJmc3n9euelZqWzJvPismOysyZlpiQx8_Gmu_8xeuelZqWzJvPismOysyZlpiQx8_Gmr3rnpWak87PlpzHzZGWjc6Zj5eJl5y13oac0ZyQko-Ki5rRi5nSnJCSj4qLmtKSmouem56LntKMng.l4LBYU48hu8uL7nbk6TVrUFwwh8E-2mtTWrBKacibxeGfX0-eub8koUKgq1drp-jSewGlWFAP31XBuvGvCOCBQ'
 admins_ids = ''
 
-maxusers = 3
+maxusers = 15
 max_gpt_tokens = 120
 count_last_msg = 4
 tokenize_url = "https://llm.api.cloud.yandex.net/foundationModels/v1/tokenizeCompletion"
